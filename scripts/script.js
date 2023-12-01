2c6e489195cc50595ba826a454d20cc2697cf6a1
Mike <teekaamichaele@gmail.co>
2023-12-01 13:24:21 +0000

    Version 1

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+const messageForm = document.getElementById("message-form");
+
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const successMessageBox = document.getElementById("success-message");
+
+    const fullNameInputTag = document.getElementById("fname");
+    const emailInputTag = document.getElementById("email");
+    const messageTextareaTag = document.getElementById("textarea");
+
+    let fullName = fullNameInputTag.value;
+    let email = emailInputTag.value;
+    let message = messageTextareaTag.value;
+
+    if (fullName && email && message) {
+        // Send to thef following values to the server to save 
+        // messageForm.submit()
+        // fullName
+        // email
+        // message 
+
+        // When the server responds true, alert the user of success
+        successMessageBox.innerHTML = ` Hello ${fullName}, your response has been recorded `
+        successMessageBox.style.display = 'block';
+
+        fullNameInputTag.value = '';
+        emailInputTag.value = '';
+        messageTextareaTag.value = '';
+
+    } else {
+        alert('ALL fields are required. Please provide them.');
+    }
+})
